6f5e5c2269e041e5b52fa27a46848489e50f47db
rd <rohandas.zero@gmail.com>
2025-02-24 11:56:33 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/chronoreel/daytech/chronoreel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.chronoreel.daytech.chronoreel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chronoreel/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChronoReelApp());
+}
+
+class ChronoReelApp extends StatelessWidget {
+  const ChronoReelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ChronoReel",
+      home: HomeScreen(),
+    );
+  }
+}
+
