dac04900d9a8555dbbe257c5290bd68291364b21
rd <rohandas.zero@gmail.com>
2025-02-24 17:27:54 +0530

    show datatype

--- lib/types/show_data.dart
+++ lib/types/show_data.dart
@@ -7,6 +7,7 @@ class ShowData {
   final String? director;
   final String? studio;
   final List<String>? starring;
+  final Map<String, dynamic> _extraData = {}; // Stores additional attributes
 
   ShowData({
     required this.title,
@@ -19,8 +20,31 @@ class ShowData {
     this.starring,
   });
 
+  // Method to add extra attributes dynamically
+  void addData(String key, dynamic value) {
+    _extraData[key] = value;
+  }
+
+  // Retrieve extra attributes dynamically
+  dynamic getAttribute(String key) => _extraData[key];
+
+  // Getter to print all stored data including extra attributes
+  Map<String, dynamic> get data {
+    return {
+      'title': title,
+      'year': year,
+      'type': type,
+      'image': image,
+      'genre': genre,
+      'director': director,
+      'studio': studio,
+      'starring': starring,
+      ..._extraData, // Include additional dynamic attributes
+    };
+  }
+
   @override
   String toString() {
-    return 'ShowData(title: $title, year: $year, image: $image, genre: $genre, director: $director, studio: $studio, starring: ${starring?.join(", ")})';
+    return 'ShowData(title: $title, year: $year, type: $type, image: $image, genre: $genre, director: $director, studio: $studio, starring: ${starring?.join(", ")}, extraData: $_extraData)';
   }
 }
